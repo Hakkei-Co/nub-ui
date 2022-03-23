66b00550cf60c7cfae7c8dc38fd849f2bd681cce
laujonat <jon@jonathanlau.io>
2022-03-22 18:21:59 -0700

    sync

--- src/.storybook/manager.js
+++ src/.storybook/manager.js
@@ -1,10 +1,13 @@
 import { addons } from '@storybook/addons';
+import { create } from "@storybook/theming/create";
+
 import customTheme from './CustomTheme';
 
 addons.setConfig({
-  theme: customTheme.light,
-  toolbar: {
-    theme: { hidden: true },
-  },
-  enableShortcuts: true,
+  theme: create({
+    base: customTheme.dark,
+    brandTitle: "Hakkei Inc.",
+    brandUrl: "https://ui.hakkei.org",
+    brandImage: customTheme.light.brandImage,
+  })
 });
