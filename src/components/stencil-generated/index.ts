11b3a9f9dd2bc205edfd1644c471b150a8aa0568
laujonat <jon@jonathanlau.io>
2022-03-30 14:56:43 -0700

    stencil

---
+++ src/components/stencil-generated/index.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'hidkb';
+
+import { defineCustomElements } from 'hidkb/loader';
+
+defineCustomElements();
+export const DeveloperTool = /*@__PURE__*/createReactComponent<JSX.DeveloperTool, HTMLDeveloperToolElement>('developer-tool');
+export const DeviceManager = /*@__PURE__*/createReactComponent<JSX.DeviceManager, HTMLDeviceManagerElement>('device-manager');
+export const KeyboardDevice = /*@__PURE__*/createReactComponent<JSX.KeyboardDevice, HTMLKeyboardDeviceElement>('keyboard-device');
+export const Ui2d = /*@__PURE__*/createReactComponent<JSX.Ui2d, HTMLUi2dElement>('ui-2d');
+export const Ui3d = /*@__PURE__*/createReactComponent<JSX.Ui3d, HTMLUi3dElement>('ui-3d');
